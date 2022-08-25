d1206c9abbff34b0709530ff7d035efbcd49eb61
Arnoldo Perez-Chavez <aperez21@huskers.unl.edu>
2022-08-25 18:45:23 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Arnoldo Perez-Chavez
+ *         aperez21@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Arnoldo Perez-Chavez!\n");
+
+  return 0;
+}
